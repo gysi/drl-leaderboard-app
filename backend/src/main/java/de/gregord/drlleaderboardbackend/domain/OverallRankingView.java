7a756f294110b4f98e1f1381e995c0aa95605790
Gregor D <github@i337.de>
2023-01-06 03:06:46 +0100

    Adds position to overallranking result

--- backend/src/main/java/de/gregord/drlleaderboardbackend/domain/OverallRankingView.java
+++ backend/src/main/java/de/gregord/drlleaderboardbackend/domain/OverallRankingView.java
@@ -1,6 +1,7 @@
 package de.gregord.drlleaderboardbackend.domain;
 
 public interface OverallRankingView {
+    Long getPosition();
     String getPlayerName();
     Long getTotalPoints();
     Long getInvalidRuns();
