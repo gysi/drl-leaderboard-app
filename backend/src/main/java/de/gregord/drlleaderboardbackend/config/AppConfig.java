6cdcac606d23aaf6868c9f2ca243e2cf1a791f41
Gregor D <github@i337.de>
2023-01-06 03:04:23 +0100

    Allows CORS Requests

--- backend/src/main/java/de/gregord/drlleaderboardbackend/config/AppConfig.java
+++ backend/src/main/java/de/gregord/drlleaderboardbackend/config/AppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collection;
 
@@ -17,6 +19,16 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
     @Bean
