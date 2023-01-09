db1f69e12bafcc6390f6b3a3b426d2b0a93e961f
Gregor D <github@i337.de>
2023-01-09 20:29:17 +0100

    Adds FAQ Page with Points System explanation.

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -8,7 +8,8 @@ const routes = [
       { name: 'overallrankings', path: 'overallrankings', component: () => import('pages/OverallPage.vue') },
       { name: 'tracks', path: 'tracks', component: () => import('pages/TracksPage.vue') },
       { name: 'tracklb', path: 'tracklb', component: () => import('pages/TrackLbPage.vue') },
-      { name: 'playerlb', path: 'playerlb', component: () => import('pages/PlayerLbPage.vue'), props: route => ({ q: route.query.q }) }
+      { name: 'playerlb', path: 'playerlb', component: () => import('pages/PlayerLbPage.vue'), props: route => ({ q: route.query.q }) },
+      { name: 'faq', path: 'faq', component: () => import('pages/FAQPage.vue') }
     ]
   },
 
