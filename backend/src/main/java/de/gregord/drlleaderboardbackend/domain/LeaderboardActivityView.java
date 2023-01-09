4a1af48c6f880d2bcdd6e4a527f2a4e74fb71442
Gregor D <github@i337.de>
2023-01-09 22:41:43 +0100

    Adds some general statistics to home/index page.

---
+++ backend/src/main/java/de/gregord/drlleaderboardbackend/domain/LeaderboardActivityView.java
@@ -0,0 +1,12 @@
+package de.gregord.drlleaderboardbackend.domain;
+
+import java.time.LocalDateTime;
+
+public interface LeaderboardActivityView {
+    String getPlayerName();
+    Long getPosition();
+    LocalDateTime getCreatedAt();
+    String getTrackName();
+    String getMapName();
+    String getParentCategory();
+}
