145039d58df510cacaba0a8361d31317aab132b8
Gregor D <github@i337.de>
2023-01-11 21:54:09 +0100

    Differentiates docker-compose ENVS for test/prod

--- server/ansible/run_playbook.sh
+++ server/ansible/run_playbook.sh
@@ -4,7 +4,7 @@ shopt -s inherit_errexit
 
 set -xe
 cd "$(dirname "$0")"
-
+cp -r env/"$1"/* app/
 ip=$(../terraform/get_server_ip.sh "$1")
 ansible-playbook --syntax-check -i "${ip}," -e "@vars/${1}_server.yaml" "$2"
 ansible-lint
