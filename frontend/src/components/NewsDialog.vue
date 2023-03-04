ec96fc58daf012fbdcdad6a681cd18184e2cc274
Gregor D <github@i337.de>
2023-03-05 00:09:26 +0100

    * Grammar corrections on the FAQ page * Adds a plus sign to the positive times when comparing them to other players on the player's lb page * Consistent use of time difference signs in the beaten by table

--- frontend/src/components/NewsDialog.vue
+++ frontend/src/components/NewsDialog.vue
@@ -13,6 +13,26 @@
         </q-banner>
         <q-separator />
         <q-card style="max-height: 40rem" class="scroll">
+<!-- 1.7.1 Release -->
+          <q-card>
+            <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
+              <div class="text-h5 q-py-sm">1.7.1 Release</div><q-space />
+              <div style="padding-top: 5px">{{ getDateDifferenceToNowByGermanTimezone('2023-03-05 00:12') }}</div>
+            </q-card-section>
+
+            <q-card-section class="q-py-sm q-pl-lg dialog-news-content">
+              <div class="text-h6">Bugfixes</div>
+              <ul class="q-my-sm" style="font-size: 1rem">
+                <li>Grammar corrections on the FAQ page
+                </li>
+                <li>Adds a plus sign to the positive times when comparing them to other players on the player's lb page
+                </li>
+                <li>Consistent use of time difference signs in the beaten by table
+                </li>
+              </ul>
+            </q-card-section>
+          </q-card>
+          <q-separator />
 <!-- 1.7.0 Release -->
           <q-card>
             <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
