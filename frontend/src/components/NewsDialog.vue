39d66562aa3624d2e99932aafa427d75afa3e57c
Gregor D <github@i337.de>
2023-03-06 00:18:59 +0100

    Release Notes 1.7.2

--- frontend/src/components/NewsDialog.vue
+++ frontend/src/components/NewsDialog.vue
@@ -13,6 +13,31 @@
         </q-banner>
         <q-separator />
         <q-card style="max-height: 40rem" class="scroll">
+<!-- 1.7.2 Release -->
+          <q-card>
+            <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
+              <div class="text-h5 q-py-sm">1.7.2 Release</div><q-space />
+              <div style="padding-top: 5px">{{ getDateDifferenceToNowByGermanTimezone('2023-03-06 00:25') }}</div>
+            </q-card-section>
+
+            <q-card-section class="q-py-sm q-pl-lg dialog-news-content">
+              <div class="text-h6">Bugfixes</div>
+              <ul class="q-my-sm" style="font-size: 1rem">
+                <li>Stops setting BETTER_ENTRY_WITH_KNOWN_DOUBLE_ACCOUNT or BETTER_ENTRY_WITH_SAME_NAME on player run if the entry it references is invalid, for example because of NO_REPLAY.
+                </li>
+                <li>Comparing entries didn't match the actual runs. This happened when the compared entries contained multiple entries for the same track (this can happen when there are multiple entries for the same player for a track).
+                </li>
+                <li>Comparing entries wouldn't match if the player had multiple entries for the same track
+                </li>
+              </ul>
+              <div class="text-h6">Misc</div>
+              <ul class="q-my-sm" style="font-size: 1rem">
+                <li>Stops showing comparing times for invalid runs
+                </li>
+              </ul>
+            </q-card-section>
+          </q-card>
+          <q-separator />
 <!-- 1.7.1 Release -->
           <q-card>
             <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
