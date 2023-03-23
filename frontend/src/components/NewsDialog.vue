8445b15171c4ff784ed132c702cb2e258deac993
Gregor D <github@i337.de>
2023-03-23 20:38:56 +0100

    Adds new invalid time category "EPIC_BUG"

--- frontend/src/components/NewsDialog.vue
+++ frontend/src/components/NewsDialog.vue
@@ -13,6 +13,22 @@
         </q-banner>
         <q-separator />
         <q-card style="max-height: 40rem" class="scroll">
+<!-- 1.7.3 Release -->
+          <q-card>
+            <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
+              <div class="text-h5 q-py-sm">1.7.3 Release</div><q-space />
+              <div style="padding-top: 5px">{{ getDateDifferenceToNowByGermanTimezone('2023-03-23 20:45') }}</div>
+            </q-card-section>
+
+            <q-card-section class="q-py-sm q-pl-lg dialog-news-content">
+              <div class="text-h6">Bugfixes</div>
+              <ul class="q-my-sm" style="font-size: 1rem">
+                <li>Adds a new invalid time category "EPIC_BUG", read more in the FAQ
+                </li>
+              </ul>
+            </q-card-section>
+          </q-card>
+          <q-separator />
 <!-- 1.7.2 Release -->
           <q-card>
             <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
