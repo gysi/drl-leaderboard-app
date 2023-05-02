2d7fc06c7a7c518e76ac3762d672aaca902aaad0
Gregor D <github@i337.de>
2023-05-02 03:44:37 +0200

    Adds purgecss to reduce css size.

---
+++ frontend/purgecss.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: ["./dist/spa/assets/*.js"],
+  css: ["./dist/spa/assets/*.css"],
+  output: ["./dist/spa/assets/"],
+  fontFace: true,
+  keyframes: true,
+  variables: false,
+  rejected: true,
+  safelist: {
+    standard: [
+      /fixed-.*/,
+      /absolute-.*/,
+      /bg-.*/,
+      /q-btn.*/,
+      /q-toggle.*/,
+      /q-icon.*/,
+      /q-item__section.*/,
+      /q-field__control.*/,
+      /q-field--filled.*/,
+      /q-table--horizontal-separator.*/,
+      /q-card__section--vert.*/,
+      /q-separator--.*/,
+      /q-tabs--.*/,
+      /q-slider--.*/,
+      /q-drawer.*/,
+      /q-batch.*/,
+      /q-table__sort.*/,
+      /map-.*/,
+      /.*slotmachine.*/,
+      /text-.*/,
+      /justify-.*/,
+      /fi-.*/],
+    greedy: [
+      /slotmachine$/
+    ],
+  }
+};
