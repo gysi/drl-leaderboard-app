47c5c454fa62f80228958b3a570077eab2b9087e
Gregor D <github@i337.de>
2023-06-24 01:03:35 +0200

    Release notes for 2.5.2

--- frontend/src/components/NewsDialog.vue
+++ frontend/src/components/NewsDialog.vue
@@ -13,6 +13,26 @@
         </q-banner>
         <q-separator />
         <q-card style="max-height: 40rem" class="scroll">
+<!-- 2.5.2 Release -->
+          <q-card>
+            <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
+              <div class="text-h5 q-py-sm">2.5.2 Release</div><q-space />
+              <div style="padding-top: 5px">{{ getDateDifferenceToNowByGermanTimezone('2023-06-24 01:08') }}</div>
+            </q-card-section>
+            <q-card-section class="q-py-sm q-pl-lg dialog-news-content">
+              <div class="text-h6">Features</div>
+              <ul class="q-my-sm" style="font-size: 1rem">
+                <li>Record all Tournament participants and entries, instead of just the top 11.
+                </li>
+              </ul>
+              <div class="text-h6">Bugfixes</div>
+              <ul class="q-my-sm" style="font-size: 1rem">
+                <li>Fixes height of tournamentpage and shows scrollbar instead.
+                </li>
+              </ul>
+            </q-card-section>
+          </q-card>
+          <q-separator />
 <!-- 2.5.1 Release -->
           <q-card>
             <q-card-section class="q-py-none q-px-sm row dialog-news-content-header">
