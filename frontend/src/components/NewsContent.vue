aa5da97521608d6fafbf6990d2ad1786eea40a23
Gregor D <github@i337.de>
2024-05-04 17:09:42 +0200

    Adds Best Position as an additional column for the ranking pages

--- frontend/src/components/NewsContent.vue
+++ frontend/src/components/NewsContent.vue
@@ -12,6 +12,21 @@
     </q-banner>
     <q-separator />
     <q-card style="overflow-wrap: break-word" class="scroll">
+      <!-- 3.5.0 Release -->
+      <q-card>
+        <q-card-section class="q-py-none q-px-sm row news-content-header">
+          <div class="text-h5 q-py-sm">3.5.0 Release</div><q-space />
+          <div style="padding-top: 5px">{{ getDateDifferenceToNowByGermanTimezone('2024-05-04 17:10') }}</div>
+        </q-card-section>
+        <q-card-section class="q-pa-sm news-content">
+          <div class="text-h6">Features</div>
+          <ul class="q-my-sm" style="font-size: 1rem">
+            <li>Adds Best Position as an additional column for the ranking pages
+            </li>
+          </ul>
+        </q-card-section>
+      </q-card>
+      <q-separator />
       <!-- 3.4.0 Release -->
       <q-card>
         <q-card-section class="q-py-none q-px-sm row news-content-header">
