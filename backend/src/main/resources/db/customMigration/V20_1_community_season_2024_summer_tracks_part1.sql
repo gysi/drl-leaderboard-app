38c92c97e0d1beaeb36bda3b77a05d562e3dcab4
Gregor D <github@i337.de>
2024-06-08 01:35:55 +0200

    Adds Green Series for Community Summer Season

--- backend/src/main/resources/db/customMigration/V20_1_community_season_2024_summer_tracks_part1.sql
+++ backend/src/main/resources/db/customMigration/V20_1_community_season_2024_summer_tracks_part1.sql
@@ -4,8 +4,14 @@
 
 -- Selected tracks spring season 2024:
 -- easy (blue series)
-INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-01] HUMBLE BEGINNINGS'), 1, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-01] HUMBLE BEGINNINGS'), 0, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-02] MODERN JUNGLE'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-03] PENCIL PUSHERS'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-04] SAND-IT!'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-05] APOLLO''S PATH'), 1, false);
+
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-06] GREEN FLAG'), 1, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-07] PAPERJAM'), 1, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-08] CASTLE CRUISE'), 1, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-09] RIPPIN'' THROUGH THE RUINS'), 1, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-10] TRIPP LITE'), 1, false);
