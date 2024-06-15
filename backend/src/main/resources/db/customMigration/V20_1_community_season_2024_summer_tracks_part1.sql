3a4abb7c1ff7cf45df61f959df357a581a1b6c8c
Gregor D <github@i337.de>
2024-06-15 02:13:33 +0200

    Yellow series release

--- backend/src/main/resources/db/customMigration/V20_1_community_season_2024_summer_tracks_part1.sql
+++ backend/src/main/resources/db/customMigration/V20_1_community_season_2024_summer_tracks_part1.sql
@@ -10,8 +10,16 @@ INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from track
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-04] SAND-IT!'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-05] APOLLO''S PATH'), 1, false);
 
+-- green series
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-06] GREEN FLAG'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-07] PAPERJAM'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-08] CASTLE CRUISE'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-09] RIPPIN'' THROUGH THE RUINS'), 1, false);
 INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-10] TRIPP LITE'), 1, false);
+
+-- yellow series
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-11] DRL HEADQUARTERS'), 2, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-12] KING''S COURT'), 2, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-13] SANDSTORM'), 2, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-14] SHADOWS AND DUST'), 2, false);
+INSERT INTO community_seasons values (4, '2024-02-SUMMER', (SELECT id from tracks where name = '[SUM24-15] COOL BREEZE'), 2, false);
