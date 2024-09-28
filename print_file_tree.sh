adfe8224703d1b43f54b73c4ca3733a6adeba6bf
Gregor D <github@i337.de>
2024-09-28 23:49:16 +0200

    excludes additional stuff from the print_file_tree.sh script because those contents aren't relevant for the gpt knowledge

--- print_file_tree.sh
+++ print_file_tree.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 tree_func_backend () {
-  tree -n --noreport -I 'imgcache|target|logs|test|protoc' backend
+  tree -n --noreport -I 'imgcache|target|logs|test|protoc|data|native-image' backend
 }
 
 tree_func_frontend () {
-  tree -n --noreport -I '.quasar|.vscode|.yarn|dist|node_modules|icons|maps' frontend
+  tree -n --noreport -I '.quasar|.vscode|.yarn|dist|node_modules|icons|maps|drone_parts|gates' frontend
 }
 
 filtering_func () {
